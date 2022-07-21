306d50d0471aa356b70d0d30915e0fbc46f85574
Atdhasiri <atdhasiri@gmail.com>
2022-07-21 11:35:28 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
+
+func reverse(s string) string {
+	rns := []rune(s) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		// swap the letters of the string,
+		// like first with last and so on.
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
+}
+
+func main() {
+	var whatIsIt string
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	whatIsIt = strings.Replace(reverse(string(sd)), ":", " ", -1)
+	fmt.Print(whatIsIt)
+}
